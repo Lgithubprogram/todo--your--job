82a8c89a1a7f29118c155f519240f67b1aae287e
Mumuni Mohammed <mumunim10@gmail.com>
2023-11-17 23:07:13 +0100

    feat: added all files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # fastapi_todo
 To-do APi with FastAPI and SQLAlchemy
+
+
+```sh
+docker run --name fastapi_todo -d \
+    -p 3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=fastapi_todo \
+    -e MYSQL_USER=fastapi_todo \
+    -e MYSQL_PASSWORD=fastapi_todo \
+    -e MYSQL_DATABASE=fastapi_todo \
+    mysql:latest
+```
+
+## Blog Post
+[https://www.ktechhub.com/tutorials/building-a-todo-api-with-fastapi-and-sqlalchemy-a-step-by-step-guide](https://www.ktechhub.com/tutorials/building-a-todo-api-with-fastapi-and-sqlalchemy-a-step-by-step-guide)
---
+++ app/core/config.py
@@ -0,0 +1,35 @@
+# app/core/config.py
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_VERSION: str = "1.0.0"
+    CONTACT: dict = {
+        "name": "KtechHub FastAPI Demo",
+        "url": "https://www.ktechhub.com/",
+        "email": "info@ktechhub.com",
+    }
+
+    ENV: str = "dev"
+
+    if ENV == "dev":
+        RELOAD: bool = True
+        LOG_LEVEL: str = "debug"
+    else:
+        RELOAD: bool = False
+        LOG_LEVEL: str = "info"
+
+    ALLOWED_HOSTS: list = ["*"]
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    class Config:
+        env_file = "./.env"
+        extra = "ignore"
+
+
+settings = Settings()
